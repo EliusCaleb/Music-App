b75f7e6085b0b8cafea654c206d8d07ea1c92c78
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-06-08 10:08:28 +0300

    creating the navbar of the app and making it responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
-import {Home} from "./router"
+import {Home, Layout} from "./router"
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route    path='/'   element= {
+                  <Layout>
+                  <Home/>
+                </Layout>
+          }
+          
+          />
+        </Routes>
+      </Router>
       <Home/>
       <h1 className='text-primary'>Hello</h1>
        
